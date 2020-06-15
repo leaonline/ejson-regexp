eca9c37d48f6a8b6400071ac1aa354ef32be8366
Jan Küster <jkuester@uni-bremen.de>
2020-06-15 22:42:39 +0200

    publish 1.1.0

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 /* eslint-env standard */
 Package.describe({
-  name: 'jkuester:ejson-regexp',
-  version: '1.0.0',
+  name: 'leaonline:ejson-regexp',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'Add RegEx as EJSON type',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:leaonline/ejson-regexp.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
