ed905558fb682bc692be0e0b6e25db422a6340a2
Jan Küster <jkuester@uni-bremen.de>
2020-06-15 22:41:58 +0200

    rewritten using extending class

--- ejson-regex.js
+++ ejson-regex.js
@@ -8,24 +8,29 @@ function getOptions (self) {
   return opts.join('')
 }
 
-RegExp.prototype.clone = function clone () {
-  const self = this
-  return new RegExp(self.source, getOptions(self))
-}
+class EJSONRegExp extends RegExp {
+  clone () {
+    const self = this
+    return new RegExp(self.source, getOptions(self))
+  }
 
-RegExp.prototype.equals = function equals (other) {
-  if (!(other instanceof RegExp)) return false
-  const self = this
-  return EJSON.stringify(self) === EJSON.stringify(other)
-}
+  equals (other) {
+    if (!(other instanceof RegExp)) return false
+    const self = this
+    return EJSON.stringify(self) === EJSON.stringify(other)
+  }
 
-RegExp.prototype.typeName = function typeName () {
-  return 'RegExp'
-}
+  typeName () {
+    return 'RegExp'
+  }
 
-RegExp.prototype.toJSONValue = function toJSONValue () {
-  const self = this
-  return { regex: self.source, options: getOptions(self) }
+  toJSONValue () {
+    const self = this
+    return {
+      regex: self.source,
+      options: getOptions(self)
+    }
+  }
 }
 
 EJSON.addType('RegExp', value => new RegExp(value.regex, value.options))
